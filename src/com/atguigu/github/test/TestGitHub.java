59b0db0e6e0c5ba25618593f6289a91f689a0a14
zzzbbbrrr <zhaoborui0528@sina.com>
2016-07-09 00:36:10 +0800

    update 02

--- src/com/atguigu/github/test/TestGitHub.java
+++ src/com/atguigu/github/test/TestGitHub.java
@@ -3,5 +3,6 @@ package com.atguigu.github.test;
 public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello github01");
+		System.out.println("hello github02");
 	}
 }
